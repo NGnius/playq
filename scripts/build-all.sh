101c68301ba0bc6d21a8450e4cf77933dff78e42
NGnius <ngniusness@gmail.com>
2019-07-04 19:44:11 -0400

    Add cross-compiling

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
-go build src/main/playq.go
+target_systems=("linux" "windows")
+target_architectures=("amd64" "amd64")
+# TODO: Fix oto (dependency?) errors for other builds
+
+if [ -d ./bin ]
+then
+  rm -rf ./bin
+fi
+
+for i in $(seq 0 $((${#target_systems[@]}-1)) )
+do
+  target_os=${target_systems[i]}
+  target_platforms=(${target_architectures[i]})
+  for j in $(seq 0 $((${#target_platforms[@]}-1)) )
+  do
+    target_plat=${target_platforms[j]}
+    output_file="./bin/playq-$target_os-$target_plat"
+    if [ $target_os == "windows" ]
+    then
+      output_file+=".exe"
+    fi
+    echo $target_os $target_plat
+    env GOOS=$target_os GOARCH=$target_plat go build -o $output_file src/main/playq.go
+    if [ $? -ne 0 ]
+    then
+      exit $?
+    fi
+  done
+done
+# go build src/main/playq.go
+echo "All targets built successfully"
+exit 0
